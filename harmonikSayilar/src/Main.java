7cec894893a1fac36a3401fa4f2f422e74d869c8
yigitortu <yigittortu287@gmail.com>
2023-12-25 09:34:29 +0300

    yorum satirlari eklendi.

--- harmonikSayilar/src/Main.java
+++ harmonikSayilar/src/Main.java
@@ -1,15 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        // dizimizi atayalım.
         int[] array = {2, 3, 4, 5, 6, 7};
 
         double sum = 0.0;
 
+        // harmonik sayılar için oluşturduğumuz 1/ formülü
         for (int i = 0; i < array.length; i++){
             sum += 1.0 / array[i];
         }
 
+        // harmonik sayı formülü
         double harmonik = sum / array.length;
 
         System.out.println("Sonuç : \t" +harmonik);
